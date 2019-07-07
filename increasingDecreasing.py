7c271f95a5c4213bec901a68e8e711e0d42b70e2
Abhishek Sisodiya <sisodiya2421@gmail.com>
2019-07-07 15:20:26 +0530

    make global instances inside function

--- increasingDecreasing.py
+++ increasingDecreasing.py
@@ -9,15 +9,15 @@ if __name__ == '__main__':
         return all(i<j for i,j in zip(itms,itms[1:]))
     def strictlyDecreasing(itms):
         return all(i>j for i,j in zip(itms,itms[1:]))
-    lb=0
-    ub=len(items)
-    mid = ((lb+ub)//2)
-    seq1 = items[lb:mid]
-    seq2 = items[mid:ub]
-    res1 = strictlyDecreasing(seq1)
-    res2 = strictlyIncreasing(seq2)
+
     def check():
-        global lb, ub, mid, seq1,seq2,res1,res2
+        lb=0
+        ub=len(items)
+        mid = ((lb+ub)//2)
+        seq1 = items[lb:mid]
+        seq2 = items[mid:ub]
+        res1 = strictlyDecreasing(seq1)
+        res2 = strictlyIncreasing(seq2)
         while(lb<ub):
             if res1 == True and res2 == True:
                 print(True)
