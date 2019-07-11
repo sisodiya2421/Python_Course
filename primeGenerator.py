65cc4cd671353805e900409802b39035f64d279c
Abhishek Sisodiya <sisodiya2421@gmail.com>
2019-07-11 21:29:56 +0530

    removed mistakes

--- primeGenerator.py
+++ primeGenerator.py
@@ -2,22 +2,21 @@ n=int(input())
 cases = []
 for i in range(n):
     cases.append(input().split(" "))
-def isPrime(x):
-    if x <= 1:
+def isPrime(n):
+    if (n <= 1):
         return False
-    if x <= 3:
+    if (n <= 3):
         return True
-    if (x%2 == 0 or x%3 ==0):
+    if (n % 2 == 0 or n % 3 == 0):
         return False
     i = 5
-    while(i * i <= x):
-        if (x % i == 0 or x % (i + 2) == 0):
+    while(i * i <= n) :
+        if (n % i == 0 or n % (i + 2) == 0) :
             return False
+        i = i + 6
     return True
 for items in cases:
     for j in range(int(items[0]),int(items[1])+1):
-        print(j)
-        print(type(j))
         if isPrime(j) == True:
             print(j,end=" ")
     print(end="\n")
