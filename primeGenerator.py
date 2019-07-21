d288ed5af4f147cdd669ac6ed1c715b1665ebe47
Abhishek Sisodiya <sisodiya2421@gmail.com>
2019-07-21 23:55:20 +0530

    prime generator program

--- primeGenerator.py
+++ primeGenerator.py
@@ -1,22 +1,21 @@
-n=int(input())
-cases = []
-for i in range(n):
-    cases.append(input().split(" "))
-def isPrime(n):
-    if (n <= 1):
-        return False
-    if (n <= 3):
-        return True
-    if (n % 2 == 0 or n % 3 == 0):
-        return False
-    i = 5
-    while(i * i <= n) :
-        if (n % i == 0 or n % (i + 2) == 0) :
-            return False
-        i = i + 6
-    return True
-for items in cases:
-    for j in range(int(items[0]),int(items[1])+1):
-        if isPrime(j) == True:
-            print(j,end=" ")
-    print(end="\n")
+try:
+    n=int(input())
+    cases = []
+    for i in range(n):
+        cases.append(input().split(" "))
+
+    for items in cases:
+        def SieveOfEratosthenes(n):
+            prime = [True for i in range(n+1)]
+            p = int(items[0])
+            while (p * p <= n):
+                if (prime[p] == True):
+                    for i in range(p * p, n+1, p):
+                        prime[i] = False
+                p += 1
+            for p in range(int(items[0]), n):
+                if prime[p]:
+                    print(p)
+        SieveOfEratosthenes(int(items[1]))
+except:
+    pass
